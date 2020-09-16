1daa41aa396f3dc99657a692bc26b5458ffa9d9d
kaphle <amritkaphle12@gmail.com>
2020-09-16 13:02:04 +0545

    added comments

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 import math
 
-#  !configure window
+
+# ?The length of the guessed is hardcorded need to fix it , other is all good
+
+#  !configure window and some variables
 pygame.init()
 WIDTH = 800
 HEIGHT = 600
@@ -34,20 +37,24 @@ LETTER_FONT = pygame.font.SysFont('comicsans', 40)
 letters = []
 
 for i in range(26):
+    # ! True determines the showing of the letter on the screen
     letters.append([chr(65+i), True])
+
 # !guessing word
+# ! if you want to change the word , you also need to change the hardcoded wining condition
 WORD = "DEVELOPING"
 guessed = []
 FONT_GUESS = pygame.font.SysFont("comicsans", 60)
 guessX = 350
 guessY = 250
 FONT_RESULT = pygame.font.SysFont('comicsans', 100)
-guessed_length = 1
+FONT_HEADER = pygame.font.SysFont('comicsans', 50)
+FONT_FOOTER = pygame.font.SysFont('comicsans', 20)
+# set = {}
 
-for i in range(1, len(WORD)-1):
-    for j in range(0, i-1):
-        if WORD[i] != WORD[j]:
-            guessed_length += 1
+# for i in range(len(WORD)-1):
+#     set.put(WORD[i])
+
 
 # !draw function called inside the game loop
 
@@ -55,8 +62,16 @@ for i in range(1, len(WORD)-1):
 def draw():
     global mouse_Y, mouse_X, letters, hangman_status, running
     screen.fill(WHITE)
-    screen.blit(hangman[hangman_status], (100, 140))
+# !putting header and footer and hangman image on the screen
+    text_header = FONT_HEADER.render("Guess The Word", 1, (33, 88, 33))
+    screen.blit(text_header, (250, 40))
 
+    text_footer = FONT_FOOTER.render("developer: Amrit kaphle", 1, (0, 0, 29))
+    screen.blit(text_footer, (310, 540))
+
+    screen.blit(hangman[hangman_status], (100, 140))
+#!
+#! displaying visible letters on the screen
     for i in range(26):
         cirX = initialX + (2*RADIUS+GAP)*(i % 13)
         cirY = initialY + (2*RADIUS+GAP)*(i//13)
@@ -71,13 +86,16 @@ def draw():
                                  math.pow((cirY - mouse_Y), 2))
             if distance < RADIUS:
                 letters[i][1] = False
-
+#!
+#! inserting the correct clicked word into the guessed array else changing the hangman image
                 if ltr in WORD:
                     guessed.append(ltr)
                 else:
                     hangman_status += 1
-
+#!
+#! showing the losing message
                 if hangman_status == 6:
+                    screen.blit(hangman[hangman_status], (100, 140))
                     text_lose = FONT_RESULT.render('You Lose', 1, (200, 0, 0))
                     screen.blit(text_lose, (300, 200))
                     running = False
@@ -89,8 +107,14 @@ while running:
 
     draw()
 
+    # !check if all letter are guessed, wining condition
+    if len(guessed) == 9:
+        text_win = FONT_RESULT.render("You Win", 1, (0, 200, 0))
+        screen.blit(text_win, (300, 200))
+        running = False
+#!
+#! if clicked letter is correct, rendering the letters else rendering _ .
     for letter in WORD:
-
         if letter in guessed:
             text_guess = FONT_GUESS.render(letter, 1, BLACK)
             screen.blit(text_guess, (guessX+15, guessY))
@@ -101,12 +125,13 @@ while running:
         guessX += 40
 
     guessX = 350
+#!
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        # mouse clicked coordinates
+        #! mouse clicked coordinates
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_X, mouse_Y = pygame.mouse.get_pos()
 
